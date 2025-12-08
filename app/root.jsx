f2b8995f977f4c4cbd8dd5ace68be8bba60ecd95
yuv881 <yuvrajsinghshaktawat28@gmail.com>
2025-12-08 09:53:37 +0530

    Setup Tailwind v4

--- app/root.jsx
+++ app/root.jsx
@@ -13,6 +13,7 @@ import favicon from '~/assets/favicon.svg';
 import {FOOTER_QUERY, HEADER_QUERY} from '~/lib/fragments';
 import resetStyles from '~/styles/reset.css?url';
 import appStyles from '~/styles/app.css?url';
+import tailwindCss from './styles/tailwind.css?url';
 import {PageLayout} from './components/PageLayout';
 
 /**
@@ -150,6 +151,7 @@ export function Layout({children}) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link rel="stylesheet" href={tailwindCss}></link>
         <link rel="stylesheet" href={resetStyles}></link>
         <link rel="stylesheet" href={appStyles}></link>
         <Meta />
--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,16 @@ import {hydrogen} from '@shopify/hydrogen/vite';
 import {oxygen} from '@shopify/mini-oxygen/vite';
 import {reactRouter} from '@react-router/dev/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [hydrogen(), oxygen(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    tailwindcss(),
+    hydrogen(),
+    oxygen(),
+    reactRouter(),
+    tsconfigPaths(),
+  ],
   build: {
     // Allow a strict Content-Security-Policy
     // withtout inlining assets as base64:
